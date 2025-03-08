475e77d79bac956e5749d928c3eb3471ce5e16c8
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-03-08 15:46:10 +0100

    Indexseite für den Demo Ordner ergänzt

---
+++ public/demo/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Webcontrol PHP Sidebar Demo</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="referrer" content="no-referrer" />
+        <meta name="apple-mobile-web-app-title" content="Biemann Controls" />
+        <link rel="icon" type="image/x-icon" href="./favicon.ico" sizes="48x48" />
+        <link rel="icon" type="image/png" href="./favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="./favicon/favicon.svg" sizes="any" />
+        <link rel="apple-touch-icon" href="./favicon/apple-touch-icon.png" sizes="180x180" />
+        <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" sizes="48x48" />
+        <link rel="manifest" href="./favicon/site.webmanifest" />
+        <link rel="stylesheet" type="text/css" href="./css/general.css" />
+    </head>
+    <body>
+        <h1>Webcontrol PHP Sidebar Demo</h1>
+        <p>Folgende Beispiele sind verfügbar:</p>
+        <a href="./static_navigation.php">Navigationsleiste links ausgerichtet, für Desktop- und Mobil-Version. Navigationsleiste kann komplett eingeklappt werden</a>
+    </body>
+</html>
